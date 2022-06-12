36db962ec36d47aff8a374b5299d81eb40adb19d
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2022-06-12 10:00:48 -0500

    Create index.js

---
+++ handler/index.js
@@ -0,0 +1,36 @@
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+const mongoose = require("mongoose");
+
+const globPromise = promisify(glob);
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+// Event handler
+const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);//looks for folder named events
+eventFiles.map((value) => require(value));
+
+//Slash Commands handler
+const slashCommands = await globPromise(
+`${process.cwd()}/SlashCommands/*/*.js`
+);
+
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
+
+client.on("ready", async () => {
+await client.application.commands.set(arrayOfSlashCommands);
+});
+
+await mongoose.connect(process.env['mongo']).then(() => console.log('Connected to mongodb'));
+};
